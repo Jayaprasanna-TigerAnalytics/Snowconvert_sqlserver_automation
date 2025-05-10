60401fa74f45199ca0d8398174f33b138ebb4637
Jayaprasanna Ganji <jayaprasanna.gan@tigeranalytics.local>
2025-05-10 23:38:51 +0530

    updated code

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\suvam.ghosh\Desktop\snowconvertPython\venv
+set VIRTUAL_ENV=C:\Users\jayaprasanna.gan\Desktop\snowconvertPython\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
